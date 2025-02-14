ac2fd78d9fef6f75d28f50dfa65f98cf5a7e8a54
Your Name <your.email@example.com>
2025-02-14 11:04:06 +0900

    ポート番号の修正

--- frontend/src/next.config.js
+++ frontend/src/next.config.js
@@ -22,7 +22,7 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "http://localhost:8080/api/:path*",
+        destination: "http://localhost:8081/api/:path*",
       },
     ];
   },
